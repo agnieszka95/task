65cedeec9258cf7d8615b3d767db242f46d27188
agnieszka95 <agnieszka.bielak@hotmail.com>
2024-02-28 10:23:54 +0100

    Create variables.tf

---
+++ terraform-code/variables.tf
@@ -0,0 +1,60 @@
+# VPC related variables
+variable "vpc_cidr_block" {
+  description = "CIDR block for the main VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for the public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for the private subnets."
+  type        = list(string)
+  default     = ["10.0.10.0/24", "10.0.11.0/24"]
+}
+
+# EKS Cluster Variables
+variable "cluster_name" {
+  description = "Name of the EKS cluster."
+  type        = string
+  default     = "my-eks-cluster"
+}
+
+variable "cluster_role_arn" {
+  description = "ARN of the IAM role used by the EKS cluster."
+  type        = string
+}
+
+variable "node_role_arn" {
+  description = "ARN of the IAM role used by the worker nodes."
+  type        = string
+}
+
+# EKS Node Group Variables
+variable "node_instance_type" {
+  description = "Instance type for the worker nodes."
+  type        = string
+  default     = "t3.medium"
+}
+
+variable "node_group_desired_size" {
+  description = "Desired number of worker nodes."
+  type        = number
+  default     = 2
+}
+
+variable "node_group_max_size" {
+  description = "Maximum number of worker nodes."
+  type        = number
+  default     = 5
+}
+
+variable "node_group_min_size" {
+  description = "Minimum number of worker nodes."
+  type        = number
+  default     = 1
+}
