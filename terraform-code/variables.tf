113a2d9ce642f6eb75d9dc6c8cc2e120ccb22d0a
agnieszka95 <agnieszka.bielak@hotmail.com>
2024-02-28 10:32:49 +0100

    Update variables.tf

--- terraform-code/variables.tf
+++ terraform-code/variables.tf
@@ -21,7 +21,6 @@ variable "private_subnet_cidrs" {
 variable "cluster_name" {
   description = "Name of the EKS cluster."
   type        = string
-  default     = "my-eks-cluster"
 }
 
 variable "cluster_role_arn" {
